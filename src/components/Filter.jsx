1a67e48eab8fca1388a85ffaa039a4c7e21d0455
Urban <urban.mutagen@gmail.com>
2025-05-22 17:06:48 +0300

    add favurite button and labels for filter component

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useId } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setFilters } from "../store/slices/carsSlice";
 import { fetchBrands } from "../services/api";
@@ -8,6 +8,9 @@ const Filter = () => {
   const dispatch = useDispatch();
   const { filters } = useSelector((state) => state.cars);
   const [brands, setBrands] = useState([]);
+  const brandId = useId();
+  const priceId = useId();
+  const kmId = useId();
 
   useEffect(() => {
     fetchBrands().then(setBrands);
@@ -20,51 +23,70 @@ const Filter = () => {
 
   return (
     <div className={css.filter}>
-      <select
-        name="brand"
-        value={filters.brand}
-        onChange={handleFilterChange}
-        className={css.checkBrand}
-      >
-        <option value="">Choose a brand</option>
-        {brands.map((brand) => (
-          <option key={brand} value={brand}>
-            {brand}
-          </option>
-        ))}
-      </select>
-      <select
-        name="rentalPrice"
-        value={filters.rentalPrice}
-        onChange={handleFilterChange}
-        className={css.checkPrice}
-      >
-        <option value="">Choose a price</option>
-        {[30, 40, 50, 60, 70].map((price) => (
-          <option key={price} value={price}>
-            {price}
-          </option>
-        ))}
-      </select>
-      <div className={css.iputWrap}>
-        <input
-          type="number"
-          name="minMileage"
-          value={filters.minMileage}
+      <div className={css.labelWrap}>
+        <label htmlFor={brandId} className={css.label}>
+          Car brand
+        </label>
+        <select
+          name="brand"
+          value={filters.brand}
           onChange={handleFilterChange}
-          placeholder="From"
-          className={css.inputFrom}
-        />
-        <input
-          type="number"
-          name="maxMileage"
-          value={filters.maxMileage}
-          onChange={handleFilterChange}
-          placeholder="To"
-          className={css.inputTo}
-        />
+          className={css.checkBrand}
+          id={brandId}
+        >
+          <option value="">Choose a brand</option>
+          {brands.map((brand) => (
+            <option key={brand} value={brand}>
+              {brand}
+            </option>
+          ))}
+        </select>
       </div>
-      <button onClick={() => dispatch(setFilters({}))}>Search</button>
+      <div className={css.labelWrap}>
+        <label htmlFor={priceId} className={css.label}>
+          Price/ 1 hour
+        </label>
+        <select
+          name="rentalPrice"
+          value={filters.rentalPrice}
+          onChange={handleFilterChange}
+          className={css.checkPrice}
+          id={priceId}
+        >
+          <option value="">Choose a price</option>
+          {[30, 40, 50, 60, 70].map((price) => (
+            <option key={price} value={price}>
+              {price}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className={css.labelWrap}>
+        <label htmlFor={kmId} className={css.label}>
+          Сar mileage / km
+        </label>
+        <div className={css.iputWrap}>
+          <input
+            type="number"
+            name="minMileage"
+            value={filters.minMileage}
+            onChange={handleFilterChange}
+            placeholder="From"
+            className={css.inputFrom}
+            id={kmId}
+          />
+          <input
+            type="number"
+            name="maxMileage"
+            value={filters.maxMileage}
+            onChange={handleFilterChange}
+            placeholder="To"
+            className={css.inputTo}
+            id={kmId}
+          />
+          <button onClick={() => dispatch(setFilters({}))}>Search</button>
+        </div>
+      </div>
     </div>
   );
 };
